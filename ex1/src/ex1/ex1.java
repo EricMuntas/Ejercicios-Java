98e51771215d6511d5d480742ea664bb2ac941c9
Eric <Eric@Gordinxi.lan>
2022-09-07 13:42:32 +0200

    Ejercicio1

---
+++ ex1/src/ex1/ex1.java
@@ -0,0 +1,16 @@
+package ex1;
+
+public class ex1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			//System.out.println("Hola");
+			System.out.println("Adios");
+			System.out.println("FIN DE PROGRAMA");
+			
+			
+			
+	}
+
+}
